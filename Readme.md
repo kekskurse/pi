c15a778396b9cfb27cab055749b71762b1f2eb2f
soeren <hello@kekskurse.de>
2018-12-13 16:17:15 +0100

    Test

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Init
+```
+sudo apt-get install git
+git clone https://github.com/sspssp/pi.git 
+cd pi
+sh init.sh
+```
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install ansible
+sudo apt-get install -y ansible
