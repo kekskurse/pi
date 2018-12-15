a9ca15d1cdab5b9dfd691dd6249ed36eb126401d
soeren <git@kekskurse.de>
2018-12-15 11:27:08 +0100

    Add SSH Stuff

--- init.sh
+++ init.sh
@@ -2,3 +2,15 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
+
+if [ -e ~/.ssh/id_rsa ]
+then
+    ssh-keygen
+fi
+
+if [ -e ~/.ssh/id_rsa ]
+then
+    ssh-keygen
+fi
+
+cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
